19dba048273ab10e5bdfb5c35593e19ad9d6e46a
abakirh <abakirhanna@gmail.com>
2021-04-17 22:14:34 -0400

    fixed some visual appearences in log in and sign up screen

--- app/src/main/java/com/example/notebook/fragments/PostsFragment.java
+++ app/src/main/java/com/example/notebook/fragments/PostsFragment.java
@@ -52,27 +52,6 @@ public class PostsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
-    protected void queryPosts() {
-        // Specify which class to query
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.setLimit(20);
-        query.addDescendingOrder(Post.KEY_CREATED_KEY);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Issue with getting posts", e);
-                    return;
-                }
-                for (Post post: posts) {
-                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
-                }
-                allPosts.addAll(posts);
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
 
     // Clean all elements of the recycler
 }
